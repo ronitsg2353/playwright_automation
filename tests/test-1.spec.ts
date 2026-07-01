55ff2f3b0d5865a4af8cd0bc595967b16c73c27a
Ronit Gaikwad <97273174+ronitsg2353@users.noreply.github.com>
2026-07-01 10:29:27 +0530

    tests/test-1.spec.ts

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,4 +1,4 @@
-import {test} from "@playwright/test"
+import {chromium,expect, Locator, test} from "@playwright/test"
 
 test('first test ',async({page})=>{
 
@@ -27,8 +27,30 @@ test("locator method with option argument",async({page})=>{
   page.locator(".inventory-item-description", { hasNotText: /sauce.*/});
 });
 
-test("locator option argument",async({page})=>{
+test("locator get by method 1",async({page})=>{
 
-  await page.goto("")
+await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+await page.getByAltText("company-branding").isVisible();
+await page.getByPlaceholder("Username",{exact:true}).fill("Admin");
+await page.getByRole('textbox', { name: 'Password' }).fill("admin123");
+await page.getByRole('button', { name: 'Login' }).click();
+await page.getByText("company-branding").isVisible();
+});
+
+
+test ("locator get by method 2",async({page})=>{
+
+await page.goto("https://www.saucedemo.com/");
+const pagetitle:String|null=await page.getByText("Swag Labs",{exact:true}).textContent();
+console.log(pagetitle);
+const username:Locator=await page.getByPlaceholder("Username");
+username.fill("standard_user");
+//await page.waitForTimeout(2000);
+const password:Locator=await page.getByPlaceholder("Password");
+password.fill("secret_sauce");
+await page.locator('[data-test="login-button"]').click();
+await page.getByAltText("Sauce Labs Backpack").isVisible();
+//await page.getByRole('button',{name:'add-to-cart-sauce-labs-backpack'}).click();
+await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
 
 });
